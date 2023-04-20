a7cdb06f1a280564bfbe90c950adeda0cc4ad7dc
Andrey Emelianov <621077@mail.ru>
2023-04-20 15:25:53 +0300

    Добавил в форму на ипнут с email невозможность ввода на кириллице и добавил маску для телефона, взяв код из предыдущего проекта

--- dist/script.js
+++ dist/script.js
@@ -76,6 +76,55 @@ class Form {
       input.value = '';
     });
   }
+  checkMailInputs() {
+    const mailInputs = document.querySelectorAll('[type="email"]');
+    mailInputs.forEach(input => {
+      input.addEventListener('keypress', function (e) {
+        if (e.key.match(/[^a-z 0-9 @ \.]/gi)) {
+          e.preventDefault();
+        }
+      });
+    });
+  }
+  initMask() {
+    let setCursosPosition = (pos, element) => {
+      element.focus();
+      if (element.setSelectionRange) {
+        element.setSelectionRange(pos, pos);
+      } else if (element.createTextRange) {
+        let range = element.createTextRange();
+        range.collapse(true);
+        range.moveEnd('character', pos);
+        range.moveStart('character', pos);
+        range.select();
+      }
+    };
+    function createMask(event) {
+      let matrix = '+1 (___) ___-____',
+        i = 0,
+        def = matrix.replace(/\D/g, ''),
+        value = this.value.replace(/\D/g, '');
+      if (def.length >= value.length) {
+        value = def;
+      }
+      this.value = matrix.replace(/./g, function (symb) {
+        return /[_\d]/.test(symb) && i < value.length ? value.charAt(i++) : i >= value.length ? '' : symb;
+      });
+      if (event.type === 'blur') {
+        if (this.value.length == 2) {
+          this.value = '';
+        }
+      } else {
+        setCursosPosition(this.value.length, this);
+      }
+    }
+    let inputs = document.querySelectorAll('[name="phone"]');
+    inputs.forEach(input => {
+      input.addEventListener('input', createMask);
+      input.addEventListener('focus', createMask);
+      input.addEventListener('blur', createMask);
+    });
+  }
   async postData(url, data) {
     let res = await fetch(url, {
       method: 'POST',
@@ -84,6 +133,8 @@ class Form {
     return await res.text();
   }
   init() {
+    this.checkMailInputs();
+    this.initMask();
     this.forms.forEach(form => {
       form.addEventListener('submit', e => {
         e.preventDefault();
